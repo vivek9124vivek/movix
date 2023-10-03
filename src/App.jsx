d64f5b368618395d1d71532059cb44a0232bf540
vivek9124vivek <vivek9124vivek@gmail.comgit config --global user.name git config --global user.email vivek9124vivek@gmail.com>
2023-10-03 12:28:19 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {fetchDataFromApi} from './utils/api';
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfigurations, getGenres } from './store/homeSlice';
-//importing components and pages
+
 
 import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Details from './pages/details/Details';
 import SearchResult from './pages/searchResult/SearchResult';
 import Explore from './pages/explore/Explore';
-
-
 import PageNotFound from './pages/404/PageNotFound';
+import Footer from './components/footer/Footer';
+
+
  
 
 
