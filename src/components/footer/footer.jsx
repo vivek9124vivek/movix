a36973d961773e272e9b57eb1d058c5c34c3e29f
vivek9124vivek <vivek9124vivek@gmail.comgit config --global user.name git config --global user.email vivek9124vivek@gmail.com>
2023-10-03 13:02:21 +0530

    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ContentWrapperimport React from "react";
 import {
     FaFacebookF,
     FaInstagram,
@@ -6,7 +6,7 @@ import {
     FaLinkedin,
 } from "react-icons/fa";
 
-import ContentWrapper from "../contectWrapper/ContentWrapper";
+import  from "../contectWrapper/ContentWrapper";
 
 import "./style.scss";
 
