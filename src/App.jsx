091bcb473a8ab788d0d033de5079c342e84165b9
vivek9124vivek <vivek9124vivek@gmail.comgit config --global user.name git config --global user.email vivek9124vivek@gmail.com>
2023-10-03 11:55:58 +0530

    app jsx error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { getApiConfigurations, getGenres } from './store/homeSlice';
 //importing components and pages
 
 import Header from './components/header/Header';
-// import Footer from './components/footer/Footer';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Details from './pages/details/Details';
